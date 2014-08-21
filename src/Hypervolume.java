ff6e7a229a6830a2a09792f2a8fb819208324b67
AdamStein593 <ams593@hotmail.co.uk>
2014-08-21 14:57:39 +0100

    Basic Calculator

--- src/Hypervolume.java
+++ src/Hypervolume.java
@@ -2,29 +2,36 @@ import java.util.Scanner;
 class Hypervolume{
     public static void main (String[]args){
         Scanner scan =new Scanner(System.in);
-  
-        float [] ListOfX = new float[5];
+        
+        
+        System.out.println("Enter numbr of solutions");
+        int NumberOfSolutions=scan.nextInt();
+        float [] ListOfX = new float[NumberOfSolutions+1];
         ListOfX[0]=0;
-        float  [] ListOfY= new float[5];
+        float  [] ListOfY= new float[NumberOfSolutions+1];
         ListOfY[0]=0;
         System.out.println("Enter solutions in accesnding order of x in the form x,y");                   
         scan.useDelimiter("[,\\s]");
-        for (int i=1;i<=4;i++){
+        for (int i=1;i<=NumberOfSolutions;i++){
             ListOfX[i] = Float.valueOf(scan.next());
             ListOfY[i] = Float.valueOf(scan.next());
         }
+        
+        System.out.println(ListValidation(ListOfX,ListOfY));
            
         float  AreaOfSolutions= CalculateArea (ListOfX,ListOfY);
         
-        float  [] XOptimal ={0,5};
-        float  [] YOptimal ={0,5};
+        System.out.println("Enter optimum in the form x,y");
+        scan.useDelimiter("[,\\s]");
+        float  [] XOptimal ={0,Float.valueOf(scan.next())};
+        float  [] YOptimal ={0,Float.valueOf(scan.next())};
         float  AreaOfOptimal= CalculateArea (XOptimal,YOptimal);
         
         float Hyper= CalculateHypervolume (AreaOfSolutions,AreaOfOptimal);
         System.out.println(Hyper);
     }
     
-    public static float  CalculateArea(float  [] x, float [] y ){
+    public static float  CalculateArea(float  [] ListOfX, float [] ListOfY ){
         // float [], float [] -> float
         // return the area underneath the point(s)
         
@@ -33,8 +40,8 @@ class Hypervolume{
         // CalculateArea({0,1.2,2.8},{0,6,4}) = 13.6
         
         float  area=0f;
-        for (int i=1;i<x.length;i++){
-            area += (x [i]-x[i-1])*y[i];
+        for (int i=1;i<ListOfX.length;i++){
+            area += (ListOfX [i]-ListOfX[i-1])*ListOfY[i];
         }
         return area;
     }
@@ -50,4 +57,16 @@ class Hypervolume{
         float HyperVolume = AreaOfSolutions/AreaOfOptimal;
         return HyperVolume;
     }
+    
+    public static boolean ListValidation(float [] ListOfX, float [] ListOfY){
+        for (int i=1;i<ListOfX.length;i++){
+            if (ListOfX [i]<=ListOfX[i-1]){
+                return false;
+            }
+            if (ListOfY [i]>=ListOfY[i-1] && i!=1){
+                return false;
+            }
+        }
+        return true;
+    } 
 }
