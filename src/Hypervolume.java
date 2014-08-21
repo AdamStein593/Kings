306f3240e9b960dc2d51943bc39e66b15a4c61fb
AdamStein593 <ams593@hotmail.co.uk>
2014-08-21 16:10:57 +0100

    Basic Hypervolume Calculator

--- src/Hypervolume.java
+++ src/Hypervolume.java
@@ -4,21 +4,23 @@ class Hypervolume{
         Scanner scan =new Scanner(System.in);
         
         boolean ListValid=false;
+        //finding the number of solutions produced by the optimisation algorithm
         System.out.println("Enter numbr of solutions");
         int NumberOfSolutions=scan.nextInt();
         float [] ListOfX = new float[NumberOfSolutions+1];
         ListOfX[0]=0;
         float  [] ListOfY= new float[NumberOfSolutions+1];
         ListOfY[0]=0;
-        
-            while (ListValid==false){
-            System.out.println("Enter solutions in accesnding order of x in the form x,y");                   
-            scan.useDelimiter("[,\\s]");
-            for (int i=1;i<=NumberOfSolutions;i++){
-                ListOfX[i] = Float.valueOf(scan.next());
-                ListOfY[i] = Float.valueOf(scan.next());
+        //The first element of the array must be 0 so there is no out of bounds error in the CalculateArea function
+        //The process of entering the solutions is repeated until they are judged to be valid
+        while (ListValid==false){
+        System.out.println("Enter solutions in accesnding order of x in the form x,y");                   
+        scan.useDelimiter("[,\\s]");
+        for (int i=1;i<=NumberOfSolutions;i++){
+            ListOfX[i] = Float.valueOf(scan.next());
+            ListOfY[i] = Float.valueOf(scan.next());
             }       
-            ListValid=ListValidation(ListOfX,ListOfY);         
+        ListValid=ListValidation(ListOfX,ListOfY);         
         }
            
         float  AreaOfSolutions= CalculateArea (ListOfX,ListOfY);
@@ -28,6 +30,8 @@ class Hypervolume{
         XOptimal[0]=0;
         float [] YOptimal =new float[2];
         YOptimal[0]=0;
+        //The first element of the array must be 0 so there is no out of bounds error in the CalculateArea function
+        //XOptimal and YOptimal must be float lists so they can be used as arguments in the CalculateArea function
         while (OptimumValid==false){
             System.out.println("Enter optimum in the form x,y");
             scan.useDelimiter("[,\\s]");
@@ -38,7 +42,7 @@ class Hypervolume{
         float  AreaOfOptimal= CalculateArea (XOptimal,YOptimal);
         
         float Hyper= CalculateHypervolume (AreaOfSolutions,AreaOfOptimal);
-        System.out.println(Hyper);
+        System.out.println("H(A): "+Hyper);
     }
     
     public static float  CalculateArea(float  [] ListOfX, float [] ListOfY ){
@@ -69,12 +73,20 @@ class Hypervolume{
     }
     
     public static boolean ListValidation(float [] ListOfX, float [] ListOfY){
+        //float [], float [] -> boolean
+        //returns true if and only if X has been entered in ascending order and Y decreases as X increases
+        
+        //ListValidation({1,2,3},{3,2,1} = true
+        //ListValidation({1,2,2},{2,2,1} = false
+        //ListValidation({3,2,1},{1,2,3} = false
+        
         for (int i=1;i<ListOfX.length;i++){
             if (ListOfX [i]<=ListOfX[i-1]){
                 System.out.println("X was not ascending, please re-enter");
                 return false;
             }
             if (ListOfY [i]>=ListOfY[i-1] && i!=1){
+                //i=1 is excluded as ListOfY[0]=0 so impossible for Y to decrease and remain positive
                 System.out.println("Y was not descending, please re-enter");
                 return false;
             }
@@ -83,6 +95,13 @@ class Hypervolume{
     }
     
     public static boolean OptimumValidation(float XOptimum, float YOptimum, float MaxX,float MaxY){
+        //float,float,float,float -> boolean
+        //returns true if and only if the X and Y value for the optimum are not lower than the max X and Y value given in the solutions
+        
+        //OptimumValidation(5,5,3,3) = true
+        //OptimumValidation(4,2,4,2) = true
+        //OptimumValidation(1,1,2,2) = false
+        
         if (XOptimum<MaxX){
             System.out.println("The Optimum for X is too low");
             return false;
