392ea4cc89866e93dbcd37d988915bafcd157fcc
AdamStein593 <ams593@hotmail.co.uk>
2014-08-22 14:59:41 +0100

    Basic Hypervolume Calculator

--- src/HypervolumeCalculation/MainHypervolume.java
+++ src/HypervolumeCalculation/MainHypervolume.java
@@ -1,6 +1,9 @@
 package HypervolumeCalculation;
+import java.util.ArrayList;
+import java.util.List;
 class MainHypervolume{
     public static void main(String[]args){
+        List list2 = new ArrayList();
         int [] sol1= {1,3};
         int [] sol2= {2,2};
         int [] sol3= {3,1};
@@ -8,9 +11,10 @@ class MainHypervolume{
         Solutions solution2 = new Solutions(sol2);
         Solutions solution3 = new Solutions(sol3);
         
-        System.out.println(solution1.area());
-        
-       
+        list2.add(solution1);
+        list2.add(solution2);
+        list2.add(solution3);
+              
     }
     
     
