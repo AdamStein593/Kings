3c66525b5328c88221a39807368c58f69fcc0fbf
AdamStein593 <ams593@hotmail.co.uk>
2014-08-26 10:54:30 +0100

    Basic Hypervolume Calculator

--- src/hyperVolume/Front.java
+++ src/hyperVolume/Front.java
@@ -18,6 +18,11 @@ public class Front {
 	}
 	public double calculateHypervolume(Solution referencePoint){
 		// TODO Actually implement algorithm
-		return 0;
+            double area=0;
+            for (int i=1;i<=listOfSolutions.size()-1; i++){
+                area+=(listOfSolutions.get(i).getX()-listOfSolutions.get(i-1).getX())*listOfSolutions.get(i).getY();
+            }
+            double hypervolume=area/(referencePoint.getX()*referencePoint.getY());
+            return hypervolume;
 	}
 }
--- src/hyperVolume/TestCalculator.java
+++ src/hyperVolume/TestCalculator.java
@@ -6,21 +6,23 @@ import java.util.List;
 public class TestCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
             List <Solution>list = new ArrayList();
-        
-        Solution solution1 = new Solution(1,3);
-        Solution solution2 = new Solution(2,2);
-        Solution solution3 = new Solution(3,1);
-        
-        Solution reference =new Solution (5,5);
-        
-        list.add(solution1);
-        list.add(solution2);
-        list.add(solution3);
-        
-        Front a =new Front("a",list);
-        double vol=a.calculateHypervolume(reference);
+            
+            Solution solution0 = new Solution(0,0);
+            Solution solution1 = new Solution(1,3);
+            Solution solution2 = new Solution(2,2);
+            Solution solution3 = new Solution(3,1);
+
+            Solution reference =new Solution (5,5);
+            
+            list.add(solution0);
+            list.add(solution1);
+            list.add(solution2);
+            list.add(solution3);
+
+            Front a =new Front("a",list);
+            double vol=a.calculateHypervolume(reference);
+            System.out.println(vol);
 
 	}
 
