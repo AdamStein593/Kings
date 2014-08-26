36b7be9cabe02d417cd52bb48fdea0b6f5546ac3
AdamStein593 <ams593@hotmail.co.uk>
2014-08-26 12:59:47 +0100

    Basic Hypervolume Calculator

--- src/hyperVolume/TestCalculator.java
+++ src/hyperVolume/TestCalculator.java
@@ -7,48 +7,36 @@ import java.io.FileReader;
 public class TestCalculator {
 
 	public static void main(String[] args) {
-            List <Solution>list = new ArrayList();
+            List <Solution>solutionList = generateList();
+                                          
+            Solution reference =new Solution (5,5);                
             
-            List <Solution>list2=list2();
-            
-            Solution solution0 = new Solution(0,0);
-            Solution solution1 = new Solution(1,3);
-            Solution solution2 = new Solution(2,2);
-            Solution solution3 = new Solution(3,1);
-
-            Solution reference =new Solution (5,5);
-            
-            //Adding the solutions to be the list
-            list.add(solution0);
-            list.add(solution1);
-            list.add(solution2);
-            list.add(solution3);          
-            
-            Front a =new Front("Algorithm A",list);
-            
+            Front a =new Front("Algorithm A",solutionList);
+                       
             double hypervolume=a.calculateHypervolume(reference);
-            System.out.println("H("+a.getSolver()+"): "+hypervolume);
-
+            System.out.println("H("+a.getSolver()+"): "+hypervolume);           
 	}
         
-        public static List<Solution>list2(){
+        public static List<Solution>generateList(){
             int i=0;
-            List <Solution>list3 = new ArrayList();
-            list3.add(new Solution(0,0));
+            List <Solution>list = new ArrayList();
+            ////The first element of the list must be (0,0) to simplify the calculateHypervolume function by not having an if statement
+            list.add(new Solution(0,0));
             try{
             BufferedReader reader = new BufferedReader(new FileReader("C:/Users/Adam/Documents/Sixth Form/Work experience and summer school/Kings/Solutions.txt"));
             String line = null;
             while ((line = reader.readLine()) != null) {
+                //Skips over i=0 as there are no solutions on this line in the file
                 if (i>0){
                     String[] parts = line.split(", ");
-                    list3.add(new Solution(Double.parseDouble(parts[0]),Double.parseDouble(parts[1])));
+                    list.add(new Solution(Double.parseDouble(parts[0]),Double.parseDouble(parts[1])));
                 }
                 i++;
             }
             }catch(Exception e){
                 System.out.println("Error");
             }
-            return list3;
+            return list;
         }
         
         
