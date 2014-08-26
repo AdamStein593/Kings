cd7d0a367570472b46b5fadf9692140a29e22397
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2014-08-26 10:10:03 +0100

    New OO structure

--- src/HypervolumeCalculation/Calculate.java
+++
@@ -1,4 +0,0 @@
-package HypervolumeCalculation;
-public class Calculate{
-    
-}
---
+++ src/hyperVolume/Front.java
@@ -0,0 +1,23 @@
+package hyperVolume;
+
+import java.util.List;
+
+/**
+ * Represents a set of solutions that make a pareto front from an algorithm.
+ * 
+ * @author Steffen Zschaler
+ * 
+ */
+public class Front {
+	private String solver;
+	private List<Solution> listOfSolutions;
+
+	public Front(String solver, List<Solution> listOfSolutions) {
+		this.solver = solver;
+		this.listOfSolutions = listOfSolutions;
+	}
+	public double calculateHypervolume(Solution referencePoint){
+		// TODO Actually implement algorithm
+		return 0;
+	}
+}
