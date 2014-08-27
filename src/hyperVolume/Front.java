cb035cd8edbc3af95b6061f624343580084d19e1
AdamStein593 <ams593@hotmail.co.uk>
2014-08-27 10:51:56 +0100

    Basic Hypervolume Calculator

--- src/hyperVolume/Front.java
+++ src/hyperVolume/Front.java
@@ -4,39 +4,41 @@ import java.util.List;
 
 /**
  * Represents a set of solutions that make a pareto front from an algorithm.
- * 
+ *
  * @author Adam Stein
- * 
+ *
  */
 public class Front {
-	private String solver;
-	private List<Solution> listOfSolutions;
 
-	public Front(String solver, List<Solution> listOfSolutions) {
-		this.solver = solver;
-		this.listOfSolutions = listOfSolutions;
-	}
+    private String solver;
+    private List<Solution> listOfSolutions;
 
-	public String getSolver() {
-		return solver;
-	}
+    public Front(String solver, List<Solution> listOfSolutions) {
+        this.solver = solver;
+        this.listOfSolutions = listOfSolutions;
+    }
 
-	/**
-	 * 
-	 * @param referencePoint TODO: Document better
-	 *            
-	 * @return the proportion that the area of the solutions take up
-	 *         when compared to the area of the optimal
-	 * 
-	 */
-	public double calculateHypervolume(Solution referencePoint) {
-		double area = 0;
-		for (int i = 1; i <= listOfSolutions.size() - 1; i++) {
-			area += (listOfSolutions.get(i).getX() - listOfSolutions.get(i - 1)
-					.getX()) * listOfSolutions.get(i).getY();
-		}
-		double hypervolume = area
-				/ (referencePoint.getX() * referencePoint.getY());
-		return hypervolume;
-	}
+    public String getSolver() {
+        return solver;
+    }
+
+    /**
+     *
+     * @param referencePoint an object which the list of solutions is compared
+     * to when calculating hypervolume
+     *
+     * @return the proportion that the area of the solutions take up when
+     * compared to the area dominated by the reference point
+     *
+     */
+    public double calculateHypervolume(Solution referencePoint) {
+        double area = 0;
+        for (int i = 1; i <= listOfSolutions.size() - 1; i++) {
+            area += (listOfSolutions.get(i).getX() - listOfSolutions.get(i - 1)
+                    .getX()) * listOfSolutions.get(i).getY();
+        }
+        double hypervolume = area
+                / (referencePoint.getX() * referencePoint.getY());
+        return hypervolume;
+    }
 }
--- src/hyperVolume/TestCalculator.java
+++ src/hyperVolume/TestCalculator.java
@@ -10,7 +10,8 @@ import java.util.Comparator;
 public class TestCalculator {
 
     public static void main(String[] args) {
-        List<Solution> solutionList = generateList();
+        String fileName="C:/Users/Adam/Documents/Sixth Form/Work experience and summer school/Kings/Solutions.txt";
+        List<Solution> solutionList = generateList(fileName);
         
         //Sorting the generated list in ascending values of X
         Collections.sort(solutionList, new Comparator<Solution>() {
@@ -47,23 +48,19 @@ public class TestCalculator {
      *
      * @return a list of Solution objects
      */
-    public static List<Solution> generateList() {
-        //i is the current line number
-        int i = 0;
+    public static List<Solution> generateList(String fileName) {
         List<Solution> list = new ArrayList();
         ////The first element of the list must be (0,0) to simplify the calculateHypervolume function by not having an if statement
         list.add(new Solution(0, 0));
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:/Users/Adam/Documents/Sixth Form/Work experience and summer school/Kings/Solutions.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line = null;
-            while ((line = reader.readLine()) != null) {
-                //Skips over i=0 as there are no solutions on this line in the file
-                if (i > 0) {
-                    String[] parts = line.split(", ");
-                    list.add(new Solution(Double.parseDouble(parts[0]), Double.parseDouble(parts[1])));
-                }
-                i++;
-            }
+            //Skips over first line as there are no solutions on this line in the file
+            line = reader.readLine();
+            while ((line = reader.readLine()) != null) {              
+                    String[] parts = line.split(", ");            
+                    list.add(new Solution(Double.parseDouble(parts[0]),Double.parseDouble(parts[1])));                  
+            }       
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
