2c92001a62e7e3c6ee332dfd922c704cd571ff02
AdamStein593 <ams593@hotmail.co.uk>
2014-09-02 15:38:03 +0100

    Basic Hypervolume Calculator

--- src/generalHypervolumeCalculator/Front.java
+++ src/generalHypervolumeCalculator/Front.java
@@ -1,4 +1,4 @@
-package hypervolumeCalculator;
+package generalHypervolumeCalculator;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- test/hypervolumeCalculatorTest/GeneralHypervolumeCalculatorTest.java
+++ test/hypervolumeCalculatorTest/GeneralHypervolumeCalculatorTest.java
@@ -4,15 +4,16 @@
  */
 package hypervolumeCalculatorTest;
 
+import generalHypervolumeCalculator.Front;
+import generalHypervolumeCalculator.Solution;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import hypervolumeCalculator.*;
 import java.util.List;
-import static hypervolumeCalculator.Calculator.generateList;
+import static generalHypervolumeCalculator.Calculator.generateList;
 import java.io.File;
 import java.util.Collections;
 import java.util.Comparator;
@@ -22,9 +23,9 @@ import java.util.ArrayList;
  *
  * @author Adam
  */
-public class hypervolumeCalculatorTest {
+public class GeneralHypervolumeCalculatorTest {
     
-    public hypervolumeCalculatorTest() {
+    public GeneralHypervolumeCalculatorTest() {
     }
     
     @BeforeClass
